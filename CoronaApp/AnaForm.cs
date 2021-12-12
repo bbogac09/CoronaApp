ab9eb683641c34a27cd3d1f5cf9cf4c59e31ba30
Boğaçhan Bakkaloğlu <bogac23@outlook.com>
2021-12-12 19:33:04 +0300

    Add project files.

---
+++ CoronaApp/AnaForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoronaApp
+{
+    public partial class AnaForm : Form
+    {
+        public AnaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void yardımToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            YardimForm form = new YardimForm();
+            form.Show();
+        }
+    }
+}
